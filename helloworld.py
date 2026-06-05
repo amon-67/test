cac3fa09e6e8dbf1ca8ea1309f16f8a9af6dc9b3
amon-67 <ushiaron@gmail.com>
2026-06-05 15:12:39 +0800

    saikai lyrics lines

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,37 @@
 print("hello world from python")
 print("this is rien")
+print("""
+ When I was young and lost
+You showed up, and had my doors unlocked
+Like threads, petals unfold
+A red Kawara-Nadeshiko
+You have shown me that I am still capable of caring for someone else
+That I still bare the same innocence inside
+'Cause now I know that pain cannot define the past
+We are built to overcome endless mishaps
+You know, it is not so bad
+When you are with me?
+Cherish as long as we last
+'Cause S is not for Sayonara
+Let memories play back
+I knew I must step up
+You deserve the world and more
+The truth is that you'd rather
+Spend our limited time together
+Protection alone is not enough
+Providing cannot fill an empty cup
+Thirsting for love
+I questioned myself a lot
+What do I know about love?
+How can I recreate what I've never had?
+All I know is that I must keep you thriving
+If nutrients are what you lack
+I will water you with every drop of blood I have
+But now I know that
+Sacrifice is the easy path
+My absence cannot ever change the fact
+I wanted the very best for you, believe me
+Our threads in red can never be cut
+And S is not for Sayonara
+Will you forgive me at last?
+""")
